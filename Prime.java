d334850a20b37cd0869e0dcab1990ca49c56af66
Nithya-997 <32432328+Nithya-997@users.noreply.github.com>
2018-07-15 20:03:34 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Primerange
+class Prime
 {
 public static void main(String args[])throws Exception
 {
@@ -8,15 +8,12 @@ boolean flag=false;
 Scanner s=new Scanner(System.in);
 System.out.println("Enter the range:");
 n=s.nextInt();
-for(int i=1;i<=n;i++)
-{
 for(int i=2;i<n/2;i++)
 {
 if(n%i==0)
 {
 flag=true;
 break;
-}
 }
 }
 if(!flag)
